f128c2d9bb837d275290d78ba183c52c335b6323
Abrams <yuanchianchen@gmail.com>
2025-08-02 14:56:22 +0800

    add: add text scroll animation in index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,9 +4,41 @@
             <h1 :class="{ active: !isMobile, inactive: isMobile }">
                 I'm<TextTyping :text="['Student', 'Developer', 'Engineer', 'Programmer']" :time="10000" />
             </h1>
-            <font-awesome-icon id="down" icon="fa-solid fa-angle-down" bounce />
         </section>
-        <section class="section2"></section>
+        <section class="section2" id="sectionIntroduction">
+            <font-awesome-icon id="down" icon="fa-solid fa-angle-up" bounce />
+            <div id="myIntro">
+                <div class="myText" id="myText1">
+                    <h3 class="myTextTitle">Who am I</h3>
+                    <p class="myTextContent">
+                        你好，我是陳元謙，一位對科技充滿熱情的大學生。目前就讀於國立中央大學太空科學與工程學系，熱愛探索新知識，並樂於動手實作各種專案。不論是寫程式、建模、設計還是組裝，我都喜歡把想法變成實體。
+                    </p>
+                </div>
+                <div class="myText" id="myText2">
+                    <h3 class="myTextTitle">Academy</h3>
+                    <p class="myTextContent">
+                        目前就讀於國立中央大學太空科學與工程學系，專注於太空科技相關領域的學習與實作。除了課堂理論外，也積極參與實作專題，像是立方衛星設計、模擬任務系統與太空資料分析等。
+                    </p>
+                </div>
+                <div class="myText" id="myText3">
+                    <h3 class="myTextTitle">Coding</h3>
+                    <p class="myTextContent">
+                        具備前後端開發經驗，熟悉使用以下工具與技術： 前端：Vue.js、Nuxt、Tailwind CSS 後端：Node.js、Express 版本控制：Git / GitLab
+                        也曾開發過團隊網站、IoT 控制介面、動畫互動頁面，並致力於將工程概念結合實際應用。
+                    </p>
+                </div>
+                <div class="myText" id="myText4">
+                    <h3 class="myTextTitle">3D 建模與列印</h3>
+                    <p class="myTextContent">
+                        擅長使用 Fusion 360 進行產品與機構建模，並使用 3D
+                        印表機（FDM）進行製作，應用於無人機機殼、立方衛星結構、簡易道具等。具備基本設計到切片、列印與後處理的完整流程經驗。
+                    </p>
+                </div>
+                <img src="" alt="" class="myImg" id="myImg1" />
+                <img src="" alt="" class="myImg" id="myImg2" />
+                <img src="" alt="" class="myImg" id="myImg3" />
+            </div>
+        </section>
     </div>
 </template>
 
@@ -32,7 +64,7 @@ onMounted(() => {
 <style scoped>
 .container {
     width: 100vw;
-    height: auto;
+    height: 1000vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -41,8 +73,7 @@ onMounted(() => {
 
 section {
     height: auto;
-    width: 92%;
-    padding: 4%;
+    width: 100%;
 }
 
 .section1 {
@@ -60,6 +91,9 @@ section {
     background-image: url("/index/bgc2.webp");
     background-size: cover;
     background-position: center;
+    position: fixed;
+    top: 0;
+    left: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -86,8 +120,174 @@ h1.inactive {
     font-size: 30px;
     color: var(--tc1);
     position: absolute;
-    bottom: 10px;
+    top: -40px;
     left: 50%;
     transform: translateX(-50%);
+}
+
+#sectionIntroduction {
+    height: 100vh;
+    position: fixed;
+    top: 100vh;
+    left: 0;
+    animation: scrollToIntro linear forwards;
+    animation-timeline: scroll();
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+#myIntro {
+    width: 80%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+}
+
+.myText {
+    width: 50%;
+    height: 100%;
+    overflow-y: auto;
+    position: absolute;
+    left: 5%;
+    top: 110vh;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+    gap: 10px;
+}
+
+.myImg {
+    width: 30%;
+    height: 80%;
+    position: absolute;
+}
+
+.myTextTitle {
+    color: var(--dc1);
+    font-size: xx-large;
+}
+
+#myText1 {
+    animation: myText1Scroll ease-in-out;
+    animation-timeline: scroll(root);
+}
+#myText2 {
+    animation: myText2Scroll ease-in-out;
+    animation-timeline: scroll(root);
+}
+#myText3 {
+    animation: myText3Scroll ease-in-out;
+    animation-timeline: scroll(root);
+}
+#myText4 {
+    animation: myText4Scroll ease-in-out;
+    animation-timeline: scroll(root);
+}
+
+@keyframes scrollToIntro {
+    0% {
+        top: 100vh;
+    }
+    10% {
+        top: 0;
+    }
+    100% {
+        top: 0;
+    }
+}
+
+@keyframes myText1Scroll {
+    0% {
+        top: 90vh;
+        opacity: 0;
+    }
+    8% {
+        top: 90vh;
+        opacity: 0;
+    }
+    13% {
+        top: 50vh;
+        opacity: 1;
+    }
+    18% {
+        top: 10vh;
+        opacity: 0;
+    }
+    100% {
+        top: -10vh;
+        opacity: 0;
+    }
+}
+
+@keyframes myText2Scroll {
+    0% {
+        top: 90vh;
+        opacity: 0;
+    }
+    16% {
+        top: 90vh;
+        opacity: 0;
+    }
+    21% {
+        top: 50vh;
+        opacity: 1;
+    }
+    26% {
+        top: 10vh;
+        opacity: 0;
+    }
+    100% {
+        top: -10vh;
+        opacity: 0;
+    }
+}
+@keyframes myText3Scroll {
+    0% {
+        top: 90vh;
+        opacity: 0;
+    }
+    24% {
+        top: 90vh;
+        opacity: 0;
+    }
+    29% {
+        top: 50vh;
+        opacity: 1;
+    }
+    34% {
+        top: 10vh;
+        opacity: 0;
+    }
+    100% {
+        top: -10vh;
+        opacity: 0;
+    }
+}
+@keyframes myText4Scroll {
+    0% {
+        top: 90vh;
+        opacity: 0;
+    }
+    32% {
+        top: 90vh;
+        opacity: 0;
+    }
+    37% {
+        top: 50vh;
+        opacity: 1;
+    }
+    42% {
+        top: 50vh;
+        opacity: 1;
+    }
+    100% {
+        top: 50vh;
+        opacity: 1;
+    }
 }
 </style>
--- app/plugins/fontawesome.js
+++ app/plugins/fontawesome.js
@@ -3,14 +3,14 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // 選擇你需要的圖示來加入 library
-import { faUser, faHome, faCircleInfo, faEnvelope, faAngleDown, faDiagramProject, faCode, faTrophy, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faHome, faCircleInfo, faEnvelope, faAngleUp, faDiagramProject, faCode, faTrophy, faBars } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram, faGithub, faGoogleDrive } from "@fortawesome/free-brands-svg-icons";
 
 library.add(
     faHome,
     faCircleInfo,
     faEnvelope,
-    faAngleDown,
+    faAngleUp,
     faGoogleDrive,
     faDiagramProject,
     faCode,
